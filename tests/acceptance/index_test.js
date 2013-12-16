34948b59cf9feb671b12040158e526ba69e3c8b6
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-12-16 05:04:15 +0000

    Fix failing test

--- tests/acceptance/index_test.js
+++ tests/acceptance/index_test.js
@@ -10,15 +10,9 @@ module('Acceptances - Index', {
 });
 
 test('index renders', function(){
-  expect(3);
-
   visit('/').then(function(){
-    var title = find('h2#title');
-    var list = find('ul li');
+    var title = find('a.navbar-brand');
 
-    equal(title.text(), 'Welcome to Ember.js');
-
-    equal(list.length, 3);
-    equal(list.text(), 'redyellowblue');
+    equal(title.text(), 'BruteHack alpha');
   });
 });
