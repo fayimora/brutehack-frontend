d45f27dcef2bf5407b58a4043522a2de3cf9bc69
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-12-19 00:13:10 +0000

    Minor cleanup

--- api-stub/routes.js
+++ api-stub/routes.js
@@ -4,7 +4,6 @@ module.exports = function(server) {
   // have to be repeated for each end point.
   server.namespace('/api', function() {
 
-    // Return fixture data for '/api/posts/:id'
     server.get('/', function(req, res) {
       res.send({"name": "Fayimora", "app": "BruteHack"});
     });
