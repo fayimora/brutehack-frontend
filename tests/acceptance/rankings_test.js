60de6b30519a68d3f8c816de8766c90784c19851
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-12-23 00:41:46 +0000

    Refine rankings page tests

--- tests/acceptance/rankings_test.js
+++ tests/acceptance/rankings_test.js
@@ -12,7 +12,12 @@ module('Acceptances - Rankings page', {
 test('renders table', function () {
   visit('/rankings').then(function() {
     var widget = find('div.rankings');
+    var widgetContent  = find('div.widget-content');
     var tableName = widget.find('div.widget-header h3').text();
+    var table = widgetContent.find("table");
+
     equal(tableName, 'Overall Rankings');
+    equal(table.find("thead > tr > th:first").text(), 'Rank', 'Rank is the first column');
   });
 });
+
