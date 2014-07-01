d8a116d044ca3cfd5de2b5a6fc935ddb536f43a6
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-01 22:28:34 +0100

    Use UTC times

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -2,5 +2,5 @@
 import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
-  return moment(value).format('dddd, Do MMMM YYYY, HH:MM A');
+  return moment.utc(value).format('dddd, Do MMMM YYYY, hh:mm A');
 });
