ff31d8cff08dbce51a5ca7d77ecb3ec17ebaed03
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-01 20:35:27 +0100

    Add missing semi-colon

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
-  return moment(value).format('dddd, Do MMMM YYYY, HH:MM A')
+  return moment(value).format('dddd, Do MMMM YYYY, HH:MM A');
 });
