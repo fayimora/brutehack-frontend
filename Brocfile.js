a4a955dd445d7fd6579f80396a5332d53f1c26a3
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-01 20:53:06 +0100

    Missing semi-colons

--- Brocfile.js
+++ Brocfile.js
@@ -17,10 +17,10 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('vendor/bootstrap-sass-official/assets/stylesheets/bootstrap.scss')
-app.import('vendor/bootstrap-sass-official/assets/javascripts/bootstrap.js')
+app.import('vendor/bootstrap-sass-official/assets/stylesheets/bootstrap.scss');
+app.import('vendor/bootstrap-sass-official/assets/javascripts/bootstrap.js');
 
 
-app.import('vendor/moment/moment.js')
+app.import('vendor/moment/moment.js');
 
 module.exports = app.toTree();
