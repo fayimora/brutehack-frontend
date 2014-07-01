82e2238e4154dda9f3e4ca7e77a057062bc1d15b
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-01 20:59:17 +0100

    Remove reference to window and use jshint globals

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,5 +1,6 @@
+/* globals moment */
 import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
-  return window.moment(value).format('dddd, Do MMMM YYYY, HH:MM A');
+  return moment(value).format('dddd, Do MMMM YYYY, HH:MM A');
 });
