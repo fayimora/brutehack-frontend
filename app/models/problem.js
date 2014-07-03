7ad9352215f3c62bfdd2574dd57339768c31d387
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-03 12:41:38 +0100

    Fix minor bug!

--- app/models/problem.js
+++ app/models/problem.js
@@ -3,8 +3,8 @@ import DS from "ember-data";
 var attr = DS.attr;
 
 var Problem = DS.Model.extend({
-  createdAt   : attr();
-  updatedAt   : attr();
+  createdAt   : attr(),
+  updatedAt   : attr(),
   author      : attr(),
   title       : attr(),
   description : attr(),
