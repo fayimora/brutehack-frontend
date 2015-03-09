dd1669febc88ea43bc921e77c633c750d0062a55
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-09 12:07:20 +0000

    I still haven't figured out what the default behaviour is so lets leave the implementation out for now

--- app/routes/problem.js
+++ app/routes/problem.js
@@ -1,9 +1,12 @@
 import Ember from "ember";
 
 var ProblemRoute = Ember.Route.extend({
-  model: function (params) {
-    return this.store.modelFor('contest').get('problems');
-  }
+  // model: function (params) {
+  //   return this.store.modelFor('contest').get('problems');
+  //   return this.store.modelFor('contest').get('problems').then(function(problems) {
+  //     return problems.findBy('id', params.problem_id);
+  //   });
+  // }
 });
 
 export default ProblemRoute;
