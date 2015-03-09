8ceb6eb5a990672a287f4ed9d991b779e9650d08
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-09 03:40:20 +0000

    Looks like I dont need to manually resolve the promise

--- app/routes/problem.js
+++ app/routes/problem.js
@@ -2,9 +2,7 @@ import Ember from "ember";
 
 var ProblemRoute = Ember.Route.extend({
   model: function (params) {
-    return this.modelFor('contest').get('problems').then(function(problems){
-      return problems.findBy('id', params.problem_id);
-    });
+    return this.store.modelFor('contest').get('problems');
   }
 });
 
