640f26b140eea62c59e4a8e6c526c1b0342bbac8
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-21 23:24:32 +0000

    Give user fields some default values

--- app/models/user.js
+++ app/models/user.js
@@ -3,14 +3,35 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-  createdAt : attr('number'),
-  updatedAt : attr('number'),
-  lastVisit : attr('number'),
   handle    : attr('string'),
+
   firstName : attr('string'),
+
   lastName  : attr('string'),
+
   email     : attr('string'),
-  rating    : attr('number'),
-  location  : attr(),
-  shirtSize : attr('string')
+
+  createdAt : attr('number', {
+    defaultValue: function() { return Date.now(); }
+  }),
+
+  updatedAt : attr('number', {
+    defaultValue: function() { return Date.now(); }
+  }),
+
+  lastVisit : attr('number', {
+    defaultValue: function() { return Date.now(); }
+  }),
+
+  rating    : attr('number', {
+    defaultValue: function() { return 0; }
+  }),
+
+  location  : attr('string', {
+    defaultValue: function() { return ""; }
+  }),
+
+  shirtSize : attr('string', {
+    defaultValue: function() { return ""; }
+  })
 });
