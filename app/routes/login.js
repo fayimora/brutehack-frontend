e984014cc538beabfa6e2334825fbda570284581
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-21 10:01:52 +0000

    /login  should only be accessible when no one is logged in

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   actions: {
     authenticate: function() {
      var credentials = this.getProperties('identification', 'password');
