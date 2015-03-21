a3b392c4af907a24524314ad7be75779722944b2
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-21 21:44:12 +0000

    Move authenticate action to login controller

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin, {
-  actions: {
-    authenticate: function() {
-     var credentials = this.getProperties('identification', 'password');
-     this.get('session').authenticate('simple-auth-authenticator:oauth2-password-grant', credentials);
-    }
-  }
-});
+export default Ember.Route.extend(UnauthenticatedRouteMixin);
