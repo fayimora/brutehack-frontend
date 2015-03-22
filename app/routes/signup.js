1dd97a1dfea1d38985d41fc23d5e84b421626ef3
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-22 23:56:07 +0000

    Clean up

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     createUser: function() {
-      var details = this.get('controller').getProperties('handle', 'firstName', 'lastName', 'password', 'email');
-      console.log(details);
+      var details = this.get('controller').getProperties('handle', 'firstName', 'lastName', 'email', 'password');
       this.store.createRecord('user', details).save();
     }
   }
