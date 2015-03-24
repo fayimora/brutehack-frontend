4464e45dd69958106b8c41d3c8fc6b9141f605c7
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-24 00:03:06 +0000

    ...

--- app/routes/problem.js
+++ app/routes/problem.js
@@ -2,10 +2,11 @@ import Ember from "ember";
 
 var ProblemRoute = Ember.Route.extend({
   // model: function (params) {
-  //   return this.store.modelFor('contest').get('problems');
-  //   return this.store.modelFor('contest').get('problems').then(function(problems) {
-  //     return problems.findBy('id', params.problem_id);
-  //   });
+    // var contest = this.store.modelFor('contest');
+    // return this.store.modelFor('contest').get('problems');
+    // return contest.get('problems').then(function(problems) {
+    //   return problems.findBy('id', params.problem_id);
+    // });
   // }
 });
 
