ef44f2270378443efc8b49818165a593f10b4fbe
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-26 00:56:02 +0000

    Goodbye login controller. Move login actions in controller to route

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  actions: {
+    authenticate: function() {
+      var controller = this.get('controller');
+      var credentials = controller.getProperties('identification', 'password');
+      this.get('session').authenticate('simple-auth-authenticator:oauth2-password-grant', credentials);
+    }
+  }
+});
